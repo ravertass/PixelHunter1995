78c00587804b945fed517557e441245c5bd7687e
Hannes Sörensson <darth.smurf@gmail.com>
2020-06-30 20:48:19 +0200

    Add to scene parsing
    Still a WIP.

--- PixelHunter1995/SceneParser.cs
+++ PixelHunter1995/SceneParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
@@ -8,34 +9,55 @@ using System.Xml;
 
 namespace PixelHunter1995
 {
+    using Dog = System.ValueTuple<float,float,float,float>;
+    class Scene
+    {
+        public string background;
+        public List<Dog> dogs;
+        public WalkingArea walkingArea;
+
+        public Scene(string background, List<Dog> dogs, WalkingArea walkingArea)
+        {
+            this.background = background;
+            this.dogs = dogs;
+            this.walkingArea = walkingArea;
+        }
+    }
     class SceneParser
     {
-        public static void ParseSceneXml(String sceneXmlPath)
+        public static Scene ParseSceneXml(String sceneXmlPath)
         {
-            // TODO: Make this function return a Scene object.
-
             XmlDocument doc = new XmlDocument();
             doc.Load(sceneXmlPath);
             XmlNodeList nodes = doc.DocumentElement.ChildNodes;
+            string background = null;
+            WalkingArea walkingArea = null;
+            List<Dog> dogs = null;
 
             foreach (XmlNode node in nodes)
             {
                 if (node.Name == "imagelayer" && node.Attributes["name"]?.InnerText == "background")
                 {
-                    // TODO: Load background
-                    Console.WriteLine("background " + node.Name);
+                    background = node.Name;  // TODO: probably not the right value
                 }
                 else if (node.Name == "objectgroup" && node.Attributes["name"]?.InnerText == "dogs")
                 {
-                    // TODO: Load dogs
-                    Console.WriteLine("dogs " + node.Name);
+                    dogs = new List<Dog>();
+                    foreach (XmlNode dogNode in node.ChildNodes)
+                    {
+                        float x = float.Parse(dogNode.Attributes["x"].Value);
+                        float y = float.Parse(dogNode.Attributes["y"].Value);
+                        float width = float.Parse(dogNode.Attributes["width"].Value);
+                        float height = float.Parse(dogNode.Attributes["height"].Value);
+                        dogs.Add((x, y, width, height));
+                    }
                 }
                 else if (node.Name == "objectgroup" && node.Attributes["name"]?.InnerText == "walking")
                 {
-                    // TODO: Add to scene
-                    Console.WriteLine(ParseWalkingXml(node));
+                    walkingArea = ParseWalkingXml(node);
                 }
             }
+            return new Scene(background, dogs, walkingArea);
         }
 
         private static WalkingArea ParseWalkingXml(XmlNode node)
