e1dee90b0e6a0b9789ddb11d4535ffe440e3168b
Hannes Sörensson <darth.smurf@gmail.com>
2020-09-08 19:15:23 +0200

    Feature/Add book sprite (#103)

--- PixelHunter1995/Content/Tileset/player.tsx
+++ PixelHunter1995/Content/Tileset/player.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.1" name="player" tilewidth="16" tileheight="32" tilecount="1" columns="1">
- <image source="../Tileset/player-position.png" width="16" height="32"/>
+ <image source="player-position.png" width="16" height="32"/>
 </tileset>
